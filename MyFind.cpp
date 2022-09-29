3826daacb9650f783e007f337184780ca512bd31
uzifuz <uzifuzi@gmx.at>
2022-09-29 18:42:45 +0200

    search function

--- MyFind.cpp
+++ MyFind.cpp
@@ -1,15 +1,39 @@
 #include <stdio.h>
+#include <string.h>
 #include <getopt.h>
 #include <stdlib.h>
 #include <assert.h>
 #include <iostream>
+#include <vector>
+#include <filesystem>
 
 using namespace std;
+using namespace filesystem;
+
+void searchFile(path const &searchPath, string const &filename, bool insensitive, bool recursive)
+{
+    auto iter = directory_iterator{searchPath};
+    auto r_iter = recursive_directory_iterator{searchPath};
+    // for (auto file : iter)
+    directory_entry file;
+    while(
+        recursive 
+        ? ++r_iter != end(r_iter)
+        : ++iter != end(iter))
+    {
+        file = recursive ? *r_iter : *iter;
+        string otherName = file.path().filename();
+        if (filename.compare(otherName) == 0 || (insensitive && strcasecmp(&filename[0], &otherName[0]) == 0))
+        {
+            cout << filename << ": " << file.path() << endl;
+        }
+    }
+}
 
 int main(int argc, char *argv[])
 {
     bool recursive = 0;
-    bool sensitive = 0;
+    bool insensitive = 0;
     int opt;
 
     while ((opt = getopt(argc, argv, "Ri")) != -1)
@@ -17,7 +41,7 @@ int main(int argc, char *argv[])
         switch (opt)
         {
         case 'i':
-            sensitive = 1;
+            insensitive = 1;
             break;
         case 'R':
             recursive = 1;
@@ -30,22 +54,31 @@ int main(int argc, char *argv[])
             break;
         }
     }
-    int options = recursive + sensitive;
+    int options = recursive + insensitive;
     if (options + 2 >= argc)
     {
         cout << "too few arguments" << endl;
         return EXIT_FAILURE;
     }
+    path searchpath = "";
+    vector<string> filenames;
     for (int iter = options + 1; iter < argc; iter++)
     {
         if (iter == options + 1)
         {
-            cout << "searchpath: " << argv[iter] << endl;
+            searchpath = argv[iter];
+            // cout << "searchpath: " << argv[iter] << endl;
         }
         else
         {
-            cout << "filename: " << argv[iter] << endl;
+            filenames.push_back(argv[iter]);
+            // cout << "filename: " << argv[iter] << endl;
         }
+    }
+
+    for (auto filename : filenames)
+    {
+        searchFile(searchpath, filename, insensitive, recursive);
     }
 
     return EXIT_SUCCESS;
