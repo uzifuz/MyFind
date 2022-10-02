bda9df513036455e7141a52c89a21aad00520a2e
uzifuz <uzifuzi@gmx.at>
2022-10-02 17:18:51 +0200

    child processes

--- MyFind.cpp
+++ MyFind.cpp
@@ -6,32 +6,43 @@
 #include <iostream>
 #include <vector>
 #include <filesystem>
+#include <unistd.h>
+#include <limits.h>
+#include <sys/wait.h>
 
 using namespace std;
 using namespace filesystem;
 
-void searchFile(path const &searchPath, string const &filename, bool insensitive, bool recursive)
+void searchFile(path const &searchPath, string const &filename, bool insensitive, bool recursive, int *pipe)
 {
     auto iter = directory_iterator{searchPath};
     auto r_iter = recursive_directory_iterator{searchPath};
     // for (auto file : iter)
     directory_entry file;
-    while(
-        recursive 
-        ? ++r_iter != end(r_iter)
-        : ++iter != end(iter))
+    string out = to_string(getpid()) + ": " + filename + ": ";
+    while (
+        recursive
+            ? ++r_iter != end(r_iter)
+            : ++iter != end(iter))
     {
         file = recursive ? *r_iter : *iter;
         string otherName = file.path().filename();
         if (filename.compare(otherName) == 0 || (insensitive && strcasecmp(&filename[0], &otherName[0]) == 0))
         {
-            cout << filename << ": " << file.path() << endl;
+            out += (string)absolute(file);
+            break;
         }
     }
+    out += +"\n";
+    auto writing = fdopen(*pipe, "w");
+    fputs(&out[0], writing);
+    fclose(writing);
+    // std::cout << filename << ": file not found" << endl;
 }
 
 int main(int argc, char *argv[])
 {
+
     bool recursive = 0;
     bool insensitive = 0;
     int opt;
@@ -57,7 +68,7 @@ int main(int argc, char *argv[])
     int options = recursive + insensitive;
     if (options + 2 >= argc)
     {
-        cout << "too few arguments" << endl;
+        std::cout << "too few arguments" << endl;
         return EXIT_FAILURE;
     }
     path searchpath = "";
@@ -76,10 +87,49 @@ int main(int argc, char *argv[])
         }
     }
 
+    int pid;
+
+    int fd[2];
+    FILE *reading;
+    char buffer[PIPE_BUF];
+    if (pipe(fd) < 0)
+    {
+        cerr << "error creating pipe" << endl;
+        exit(EXIT_FAILURE);
+    }
+
     for (auto filename : filenames)
     {
-        searchFile(searchpath, filename, insensitive, recursive);
+        pid = fork();
+        switch (pid)
+        {
+        case -1:
+            cerr << "fork error" << endl;
+            break;
+        case 0:
+            // child
+            close(fd[0]); // close reading end
+            searchFile(searchpath, filename, insensitive, recursive, &fd[1]);
+            exit(EXIT_SUCCESS);
+        default:
+            // parent
+            // std::cout << "fork: " << pid << " " << filename << endl;
+            break;
+        }
     }
+
+    close(fd[1]);
+    reading = fdopen(fd[0], "r");
+
+    // std::cout << getpid() << ") children: " << childPids.size() << endl;
+    while (wait(NULL) != -1)
+        ; // wait for children to terminate
+
+    while (fgets(buffer, PIPE_BUF, reading) != NULL)
+    {
+        std::cout << buffer;
+    }
+    fclose(reading);
 
     return EXIT_SUCCESS;
 }
